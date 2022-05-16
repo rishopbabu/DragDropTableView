add7356a0ea6efc58985b08b2bffcbbfcd5857bf
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-05-16 19:22:53 +0530

    Initial Commit

---
+++ DragDropTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragDropTableView
+//
+//  Created by MAC-OBS-26 on 16/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
