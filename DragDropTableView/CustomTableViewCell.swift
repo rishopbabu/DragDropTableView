71f2da88d2ab52dc3c938c0e5cbb62e717e34324
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-05-19 20:02:47 +0530

    first commit

---
+++ DragDropTableView/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  DragDropTableView
+//
+//  Created by MAC-OBS-26 on 16/05/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
